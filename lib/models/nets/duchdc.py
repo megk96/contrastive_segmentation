4572c4ade4a449230efe6d1fbf426ab84bdc2017
meghana <sonamegz@gmail.com>
2021-09-02 16:05:27 +0100

    testing duchdc

--- lib/models/nets/duchdc.py
+++ lib/models/nets/duchdc.py
@@ -79,7 +79,7 @@ class DUCHDC(nn.Module):
             self.layer4[idx].conv2.dilation = (layer4_group_config[idx], layer4_group_config[idx])
             self.layer4[idx].conv2.padding = (layer4_group_config[idx], layer4_group_config[idx])
 
-        self.duc = _DenseUpsamplingConvModule(8, 2048, self.num_classes)
+        self.duc = _DenseUpsamplingConvModule(1, 2048, self.num_classes)
 
         initialize_weights(self)
 

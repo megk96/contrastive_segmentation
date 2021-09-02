027d9ca438082b6a0b4ca9fe1351863f4d47665d
meghana <sonamegz@gmail.com>
2021-09-02 15:55:40 +0100

    testing duchdc

--- lib/models/nets/duchdc.py
+++ lib/models/nets/duchdc.py
@@ -136,10 +136,15 @@ class DUCHDC_CONTRAST(nn.Module):
 
 
     def forward(self, x,  with_embed=False, is_eval=False):
+        print(x.shape)
         x = self.layer0(x)
+        print(x.shape)
         x = self.layer1(x)
+        print(x.shape)
         x = self.layer2(x)
+        print(x.shape)
         x = self.layer3(x)
+        print(x.shape)
         features = self.layer4(x)
         out = self.duc(features)
         embeddings = self.proj_head(features)
